4dd6b89c173393dd9d4fd39de78c2deb8e334004
Hash52 <hirokinoakaunto@gmail.com>
2018-04-09 16:35:05 +0900

    add: websocket

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
-# Gemfile
 source 'https://rubygems.org'
 
 gem 'http'
 gem 'json'
+gem 'faye-websocket'
+gem 'eventmachine'
+gem 'slack-api'
--- test.rb
+++ test.rb
@@ -2,5 +2,13 @@
 require 'http'
 require 'json'
 
-response = HTTP.post("https://slack.com/api/api.test")
+channel_hashi = "D9BN2P7GX"
+
+response = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+    token: ENV['SLACK_MY_API_TOKEN'],
+    channel: channel_hashi,
+    text: "わたしぶーちゃん？",
+  #  as_user: true,
+  })
+puts ENV['SLACK_API_TOKEN']
 puts JSON.pretty_generate(JSON.parse(response.body))
